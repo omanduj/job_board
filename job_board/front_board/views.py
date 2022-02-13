c2b9111e02c3c34412d8146b1f00c3e4de6967e9
omanduj <omanduj@gmail.com>
2022-02-12 18:14:24 -0600

    Sets up initial endpoint and declares PostgresDB

--- job_board/front_board/views.py
+++ job_board/front_board/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+def demo():
+    return JSONresponse({'1': 'test'})
--- job_board/job_board/urls.py
+++ job_board/job_board/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from front_board.views import demo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mainboard', demo),
 ]
